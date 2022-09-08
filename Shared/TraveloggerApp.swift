1b0011e6e3be3585239a12d770c6534accaaf4e0
DJKramer314 <djkramer314@gmail.com>
2022-09-07 20:45:12 -0500

    Initial Commit

---
+++ Shared/TraveloggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraveloggerApp.swift
+//  Shared
+//
+//  Created by Dylan on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TraveloggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
